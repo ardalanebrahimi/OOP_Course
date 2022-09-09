cb7dbe0104fb16a74a49f96011c7b4d8b9f80175
ardalan ebrahimi <ardalan.ebrahimi@gmail.com>
2022-09-09 23:23:47 +0200

    finish ocp sample

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
-﻿namespace OOP
+﻿using System.Collections.Generic;
+
+namespace OOP
 {
     class Program
     {
         static void Main(string[] args)
         {
-            IProperty[] properties = PropertyService.SearchProperties();
+            IList<IProperty> properties = PropertyService.SearchProperties();
 
             foreach (IProperty prop in properties)
                 System.Console.WriteLine(prop.Print());
--- Services/PropertyService.cs
+++ Services/PropertyService.cs
@@ -1,23 +1,41 @@
-﻿namespace OOP
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OOP
 {
-    class PropertyService
+    public class PropertyService
     {
-        public static IProperty[] SearchProperties()
+        public static IList<IProperty> SearchProperties()
         {
-            IProperty[] properties = new Property[]{
+            return PropertyService.SearchPropertiesInternal(x => true);
+        }
+        protected static IList<IProperty> SearchProperties(Func<IProperty, bool> filter)
+        {
+            return PropertyService.SearchPropertiesInternal(filter);
+        }
+        
+        internal static IList<IProperty> SearchPropertiesInternal(Func<IProperty, bool> filter)
+        {
+            IList<IProperty> properties = new List<IProperty>{
                 new Apartment() { Area = 100, Address = Address.Zone1 },
                 new Apartment() { Area = 200, Address = Address.Zone2  },
                 new Apartment() { Area = 120, Address = Address.Zone2  },
                 new Apartment() { Area = 120, Address = Address.Zone3  },
-                
+
                 new House() { Area = 120, WholeArea = 300, Address = Address.Zone3  },
                 new House() { Area = 90, WholeArea = 200, Address = Address.Zone4  },
-                
+
                 new Warehouse() { Area = 300, Height = 6, Address = Address.Zone1  },
                 new Warehouse() { Area = 300, Height = 5, Address = Address.Zone2  },
                 new Warehouse() { Area = 400, Height = 4, Address = Address.Zone3  },
             };
-            return properties;
+            return properties.Where(filter).ToList();
         }
+
+        //private static Func<IProperty, bool> NewMethod()
+        //{
+        //    return x => x.Area > 150;
+        //}
     }
 }
